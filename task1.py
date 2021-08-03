75eff97ec87171fe3ae155f809025952b10b671b
Vova Rybnikov <vofish@gmail.com>
2021-08-03 18:15:22 +0300

    Add task1

---
+++ task1.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+# Python ver. 3.8.10
+
+"""
+1. Detect locally mounted disk (make sure it is local) with at least X MB free space, create Z files of size Y,
+run Z “dd” processes which where each process will fill the selected file with Data and print time took to complete the work.
+"""
+
+
+import psutil
+import time
+import threading
+import subprocess
+
+x = 200   # free space on disk in MB
+y = 15    # file size in MB
+z = 10    # numbers of files
+
+# Default command to run
+cmd = ['dd', 'if=/dev/zero', 'of=file.dat', 'bs=2M', 'count=1', 'status=none']
+
+
+def dd_func(file_size,file_number):
+  # Default command to run
+  cmd = ['dd', 'if=/dev/zero', 'of=file.dat', 'bs=2M', 'count=1', 'status=none']
+  # Update file name
+  cmd[2] = 'of=file' + str(file_number) + '.dat'
+  # Update file size
+  cmd[3] = 'bs=' + str(file_size) + 'M'
+  subprocess.Popen(cmd)
+
+def get_free_space(partition):
+    # Convert free space to MB and round
+    free_space = round(psutil.disk_usage(partition).free/1024.0/1024.0)
+    return free_space
+
+if __name__ == "__main__":
+  start_time = time.time()
+
+  thread_list = []
+
+  # Get all mounted partitions
+  partitions = psutil.disk_partitions(all=False)
+  # Check free space on each local device
+  for p in partitions:
+    if p.fstype not in ('nfs', 'smb') and get_free_space(p.mountpoint) >= x:
+      print (f"Disk {p.device} has free {get_free_space(p.mountpoint)}MB")
+      
+  for i in range(z):
+    thread = threading.Thread(target=dd_func, args=(y,i))
+    thread_list.append(thread)
+  
+  for thread in thread_list:
+    thread.start()
+
+  for thread in thread_list:
+    thread.join()
+      
+  end_time = time.time()
+
+  print(f"Time took to complete the work {end_time - start_time} seconds")
